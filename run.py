bdfe2385159b9af3e4368458232cf22c335b9e83
std67122420323-tech <std.67122420323@ubru.ac.th>
2026-03-11 03:07:34 +0700

    upd

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
-from shoe import app, db  
+import os
+from shoe import app, db
 
 if __name__ == '__main__':
     with app.app_context():
-        db.create_all()  
-    app.run(debug=True, host='0.0.0.0') 
+        db.create_all()
+    
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
